4da25a2594e8d43d773997362734d8eff4c7197f
Guang-Lin Wei <guanglinwei@gmail.com>
2024-04-20 16:41:22 -0400

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
     return (
         <div className="App">
-            Hello world
+            <div className="underline">
+                Hello world
+            </div>
         </div>
     );
 }
