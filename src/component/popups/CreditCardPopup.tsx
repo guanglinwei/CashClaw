46cc7e235136a3f11529976234975331f3c82eab
Guang-Lin Wei <guanglinwei@gmail.com>
2024-04-20 23:04:48 -0400

    popup example

---
+++ src/component/popups/CreditCardPopup.tsx
@@ -0,0 +1,13 @@
+import Popup from '../Popup';
+
+function CreditCardPopup() {
+    return (
+        <Popup title='Credit Card' content={(
+            <div>
+                Put content here
+            </div>
+        )} />
+    );
+}
+
+export default CreditCardPopup;
